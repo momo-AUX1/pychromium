4b3ebf89959481918d75db6bf75b7f254787f436
momo-AUX1 <102610940+momo-AUX1@users.noreply.github.com>
2024-03-18 23:03:34 +0100

    Update chromium_qt6.py

--- chromium_qt6.py
+++ chromium_qt6.py
@@ -755,9 +755,9 @@ def about_handler():
 
 def feature_to_string(feature):
     feature_map = {
-        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaAudioCapture: "access to your microphone",
-        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaVideoCapture: "access to your camera",
-        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaAudioVideoCapture: "access to your microphone and camera",
+        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaAudioCapture: "have access to your microphone",
+        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaVideoCapture: "have access to your camera",
+        PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaAudioVideoCapture: "have access to your microphone and camera",
         PyQt6.QtWebEngineCore.QWebEnginePage.Feature.Notifications: "send notifications",
         PyQt6.QtWebEngineCore.QWebEnginePage.Feature.Geolocation: "access your location",
         PyQt6.QtWebEngineCore.QWebEnginePage.Feature.MediaAudioCapture: "access your microphone",
@@ -776,7 +776,7 @@ def permissions_handler(url, feature):
     feature_description = feature_to_string(feature)
     user_response = QMessageBox.question(None, 
                                          "Permission Request", 
-                                         f"Are you sure you want {page_title} to have {feature_description}?", 
+                                         f"Are you sure you want {page_title} to {feature_description}?", 
                                          QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, 
                                          QMessageBox.StandardButton.No)
 
